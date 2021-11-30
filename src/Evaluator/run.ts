a6c3e65fabcbf4130e8f905b3edf86013c74f45e
ENvironmentSet <sbshw1@gmail.com>
2021-11-30 22:14:02 +0900

    run

--- src/Evaluator/run.ts
+++ src/Evaluator/run.ts
@@ -4,12 +4,15 @@ import { RunoProgram } from '../Parser/AST';
 import { Environment } from './Environment';
 import { evalStatement } from './statement';
 
-export function run(prims: Record<string, RunoValue>, drivers: Record<string, (x: RunoValue) => void>, statements: RunoProgram): RunoError | void {
+export function run(prims: Record<string, RunoValue>, drivers: Record<string, (x: RunoValue) => void>, statements: RunoProgram, onError: (e: RunoError) => void): void {
   const toplevelEnv = new Environment(prims, undefined, drivers);
 
   for (const statement of statements) {
     const evalResult = evalStatement(toplevelEnv, statement);
 
-    if (evalResult) return evalResult;
+    if (evalResult) {
+      onError(evalResult);
+      return;
+    }
   }
 }
