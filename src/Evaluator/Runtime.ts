389e5db916118820ad5a78b922b3b52a749aebd4
ENvironmentSet <sbshw1@gmail.com>
2021-12-07 23:51:34 +0900

    beta

--- src/Evaluator/Runtime.ts
+++ src/Evaluator/Runtime.ts
@@ -1,4 +1,6 @@
-import { RunoValue } from './Value';
+import { isRunoCallable, isRunoCustomValue, isRunoTuple, RunoValue } from './Value';
+import BigNumber from 'bignumber.js';
+import { Cell, Stream } from 'sodiumjs';
 
 const RunoErrorTag: unique symbol = Symbol('@RunoErrorTag');
 export type RunoError = { [RunoErrorTag]: typeof RunoErrorTag, message?: string; }
@@ -13,6 +15,25 @@ export function isRunoError(x: RunoValue | RunoError): x is RunoError {
 }
 export function composeError(x: RunoError, y: RunoError): RunoError {
   return createError(`${x.message} \n ${y.message}`);
+}
+
+export function resolveType(value: RunoValue): string {
+  if (typeof value === 'string') return 'text';
+  if (value instanceof BigNumber) return 'number';
+  if (typeof value === 'boolean') return 'boolean';
+  if (isRunoTuple(value)) return 'value';
+  if (isRunoCallable(value)) return 'function';
+  if (isRunoCustomValue(value)) return value.tag;
+  if (value instanceof Cell) return 'cell';
+  if (value instanceof Stream) return 'stream';
+  else throw new Error('unreachable code');
+}
+
+export function createTypeError(value: RunoValue, expectedType: string, paramName?: string, funcName?: string) {
+  if (paramName && funcName) {
+    return createError(`value of type ${expectedType} expected as '${paramName}' of ${funcName}, but got value of type ${resolveType(value)}`)
+  }
+  else return createError(`value of type ${expectedType} expected, but got value of type ${resolveType(value)}`)
 }
 
 export type RunoEvalResult = RunoValue | RunoError;
